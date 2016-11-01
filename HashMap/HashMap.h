86dfef27752f0174c60a12848be05a3f6284915b
Benoit Letondor <benoit.letondor@gmail.com>
2016-11-01 13:45:09 +0100

    Add xcode proj

---
+++ HashMap/HashMap.h
@@ -0,0 +1,19 @@
+//
+//  HashMap.h
+//  HashMap
+//
+//  Created by Benoit Letondor on 01/11/2016.
+//  Copyright © 2016 Benoit Letondor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HashMap.
+FOUNDATION_EXPORT double HashMapVersionNumber;
+
+//! Project version string for HashMap.
+FOUNDATION_EXPORT const unsigned char HashMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HashMap/PublicHeader.h>
+
+
